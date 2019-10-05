7459db4962583f8cea9378220ca28436e74adbdd
Avijeet Pandey <avijeetpandey25@gmail.com>
2019-10-05 13:46:41 +0530

    fixed port related issue

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const path=require('path');
 const app=express();
-const port=3000;
+
+const port =process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -11,7 +12,5 @@ app.get('/',(req,res)=>{
 })
 
 
-app.listen(process.env.port,()=>console.log('Server is up and running '));
+app.listen(port,()=>console.log('Server is up and running '));
 
-
-module.exports=app;
