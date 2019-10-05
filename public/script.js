01ae6bafc01a27b226dea4827b42754e1ce9a128
Avijeet Pandey <avijeetpandey25@gmail.com>
2019-10-05 14:12:18 +0530

    added authentication to the main script

--- public/script.js
+++ public/script.js
@@ -1,11 +1,18 @@
 
+//function to fetch the response from the NumbersAPI
+
+
 const getNumberResponse = () => {
     let number=document.getElementById('search');
-   
+
+
+    //checking if the entered value is a number or not 
+    if(!isNaN(number.value)){
 
   const url = `http://numbersapi.com/${number.value}/math?json`;
 
 
+      //fetching the data from the API
 
   fetch(url)
     .then(data => data.json())
@@ -19,10 +26,16 @@ const getNumberResponse = () => {
       document.getElementById('cards').style.visibility='visible';
     })
     .catch(err => console.log(err));
+  } //handling if the entered value is not a number
+  else {
+    window.alert('Please enter a valid number .');
+    closeIt();
+    number.value="";
+  }
 };
 
 
-
+//after clicking the button the card will be closed // visibility will be hidden 
 const closeIt=()=>{
   document.getElementById('cards').style.visibility='hidden';
 }
